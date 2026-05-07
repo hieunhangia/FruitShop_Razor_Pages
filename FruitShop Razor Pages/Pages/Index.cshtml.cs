61fc26f38956412fa91882031047c4fb36293f3c
HieuNhanGia <hieuvippro531@gmail.com>
2026-05-07 16:37:56 +0700

    initialize project files

---
+++ FruitShop Razor Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FruitShop_Razor_Pages.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ FruitShop Razor Pages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FruitShop_Razor_Pages.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
