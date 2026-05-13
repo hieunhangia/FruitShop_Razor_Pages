d2d77e46d477caba268f32c62d229f4e995d095e
HieuNhanGia <hieuvippro531@gmail.com>
2026-05-13 08:16:43 +0700

    update mapping strategies to required in target

--- Service/DTOs/Customer/Cart/CartMapper.cs
+++ Service/DTOs/Customer/Cart/CartMapper.cs
@@ -4,7 +4,7 @@ using Riok.Mapperly.Abstractions;
 
 namespace Service.DTOs.Customer.Cart;
 
-[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
 public partial class CartMapper
 {
     [MapProperty($"{nameof(CartItem.Product)}.{nameof(Product.ProductUnit)}.{nameof(ProductUnit.Name)}",
