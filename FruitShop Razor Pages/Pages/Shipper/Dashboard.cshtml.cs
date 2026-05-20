078bce45291b1a7726381b36eae576cef0a5a70f
HieuNhanGia <hieuvippro531@gmail.com>
2026-05-20 11:29:44 +0700

    Refactor menu items and add "Shipper Dashboard" page

---
+++ FruitShop Razor Pages/Pages/Shipper/Dashboard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FruitShop_Razor_Pages.Pages.Shipper;
+
+public class DashboardModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
